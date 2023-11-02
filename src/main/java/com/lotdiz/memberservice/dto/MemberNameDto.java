c1bd843fe45111d0d4c40e22d8966a294972f165
nowgnas <dev.nowgnas@gmail.com>
2023-11-02 19:58:19 +0900

    add: add get member name

---
+++ src/main/java/com/lotdiz/memberservice/dto/MemberNameDto.java
@@ -0,0 +1,17 @@
+package com.lotdiz.memberservice.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberNameDto {
+
+    private Long memberId;
+    private String memberName;
+}
